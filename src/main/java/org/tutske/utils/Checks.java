45ce98d0d1d696704a8a2df4c4f5bcb86a735341
Sarchy Tutske <sarchy.tutske@gmail.com>
2017-05-18 07:15:04 +0200

    [UTL-2] Add Check class with basic matchers.

---
+++ src/main/java/org/tutske/utils/Checks.java
@@ -0,0 +1,155 @@
+package org.tutske.utils;
+
+import java.util.Collection;
+
+
+public class Checks {
+
+	public static <T> void assure (Matcher<T> matcher, T ... values) {
+		for ( T value : values ) {
+			if ( ! matcher.matches (value) ) {
+				throw new IllegalArgumentException (formatError (value, matcher));
+			}
+		}
+	}
+
+	public static <T> void check (T value, Matcher<T> matcher) {
+		assert matcher.matches (value) : formatError (value, matcher);
+	}
+
+	public static Matcher<Object> notNull () {
+		return not (nullValue ());
+	}
+
+	public static <T> Matcher<T> is (final T expected) {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				return expected.equals (value);
+			}
+			@Override public String describe () {
+				return "a value matching " + expected;
+			}
+		};
+	}
+
+	public static Matcher<Object> nullValue () {
+		return new Matcher<Object> () {
+			@Override public boolean matches (Object value) {
+				return value == null;
+			}
+			@Override public String describe () {
+				return "a null value";
+			}
+		};
+	}
+
+	public static <T> Matcher<T> not (final Matcher<T> matcher) {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				return ! matcher.matches (value);
+			}
+			@Override public String describe () {
+				return "not " + matcher.describe ();
+			}
+		};
+	}
+
+	public static <T extends Number> Matcher<T> between (final T lower, final T upper) {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				return value != null && value.doubleValue () >= lower.doubleValue () && value.doubleValue () < upper.doubleValue ();
+			}
+			@Override public String describe () {
+				return "a number between " + lower.doubleValue () + " (inclusive) and " + upper.doubleValue () + " (exclusive)";
+			}
+		};
+	}
+
+	public static Matcher<String> nonEmptyString () {
+		return not (emptyString ());
+	}
+
+	public static Matcher<String> emptyString () {
+		return new Matcher<String> () {
+			@Override public boolean matches (String value) {
+				return value != null && value.isEmpty ();
+			}
+			@Override public String describe () {
+				return "an empty string";
+			}
+		};
+	}
+
+	public static <T extends Collection> Matcher<T> notEmpty () {
+		return not (empty ());
+	}
+
+	public static <T extends Collection> Matcher<T> empty () {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				return value.isEmpty ();
+			}
+			@Override public String describe () {
+				return "an empty collection";
+			}
+		};
+	}
+
+	public static <T> Matcher<T> allOf (final Matcher<T> ... matchers) {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				for ( Matcher<T> matcher : matchers ) {
+					if ( ! matcher.matches (value) ) { return false; }
+				}
+				return true;
+			}
+			@Override public String describe () {
+				StringBuilder builder = new StringBuilder ();
+				for ( Matcher<T> matcher : matchers ) {
+					builder.append (", and is ").append (matcher.describe ());
+				}
+				builder.delete (0, 9).insert (0, "a value that is ");
+				return builder.toString ();
+			}
+		};
+	}
+
+	public static <T> Matcher<T> anyOf (final Matcher<T> ... matchers) {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				for ( Matcher<T> matcher : matchers ) {
+					if ( matcher.matches (value) ) { return true; }
+				}
+				return false;
+			}
+			@Override public String describe () {
+				StringBuilder builder = new StringBuilder ();
+				for ( Matcher<T> matcher : matchers ) {
+					builder.append (", or is ").append (matcher.describe ());
+				}
+				builder.delete (0, 8).insert (0, "a value that is ");
+				return builder.toString ();
+			}
+		};
+	}
+
+	public static <S, T extends Collection<S>> Matcher<T> containing (final Matcher<S> matcher) {
+		return new Matcher<T> () {
+			@Override public boolean matches (T value) {
+				for ( S s : value ) {
+					if ( matcher.matches (s) ) { return true; }
+				}
+				return false;
+			}
+			@Override public String describe () {
+				return "a collection with an element that matches " + matcher.describe ();
+			}
+		};
+	}
+
+	private static <T> String formatError (T value, Matcher<T> matcher) {
+		return String.format ("Argument did not validate\nexpected: %s\nbut got: %s",
+			matcher.describe (), matcher.describeMismatch (value)
+		);
+	}
+}
