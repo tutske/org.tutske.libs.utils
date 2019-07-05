69380f38d9c58aa4e183664e72b01befc247a7e4
Groot Sarchy <groot.sarchy@gmail.com>
2019-07-05 14:45:33 +0200

    [*] Use assertThat from hamcrest.

--- src/test/java/org/tutske/lib/utils/HexDecodeTest.java
+++ src/test/java/org/tutske/lib/utils/HexDecodeTest.java
@@ -1,7 +1,7 @@
 package org.tutske.lib.utils;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
--- src/test/java/org/tutske/lib/utils/ResourceTest.java
+++ src/test/java/org/tutske/lib/utils/ResourceTest.java
@@ -1,6 +1,6 @@
 package org.tutske.lib.utils;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
